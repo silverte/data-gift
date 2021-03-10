97eea2d2069416f91c9b773947f75cd40acbb01a
silverte <silverte@sk.com>
2021-03-10 21:50:11 +0900

    init

---
+++ src/main/java/com/sktelecom/product/DataGiftApplication.java
@@ -0,0 +1,13 @@
+package com.sktelecom.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataGiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sktelecom/product/DataGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sktelecom.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

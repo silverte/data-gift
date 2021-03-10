8d3d7263f7c3537ebf99dee5c7416fd3747d5bbd
silverte <silverte@sk.com>
2021-03-10 22:09:19 +0900

    add rest controller

---
+++ src/main/java/com/sktelecom/product/datagift/controller/web/DataGiftWebController.java
@@ -0,0 +1,8 @@
+package com.sktelecom.product.datagift.controller.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class DataGiftWebController {
+	//system.out.println("hello world");
+}

da22150c0b862c2020839061b6d46a93582fbbf7
silverte <silverte@sk.com>
2021-03-14 13:17:42 +0900

    add config

--- src/main/java/com/sktelecom/product/DataGiftApplication.java
+++ src/main/java/com/sktelecom/product/DataGiftApplication.java
@@ -3,11 +3,16 @@ package com.sktelecom.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class DataGiftApplication {
 
 	public static void main(String[] args) {
+		log.debug("hi~~");
 		SpringApplication.run(DataGiftApplication.class, args);
+		
 	}
 
 }
--- src/main/java/com/sktelecom/product/datagift/controller/web/DataGiftWebController.java
+++ src/main/java/com/sktelecom/product/datagift/controller/web/DataGiftWebController.java
@@ -1,8 +1,21 @@
 package com.sktelecom.product.datagift.controller.web;
 
-import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController 
+@RequestMapping("/v1/api/rest")
 public class DataGiftWebController {
-	//system.out.println("hello world");
+	
+	@GetMapping("harry")
+	public String dataGiftCount(){
+	   log.info("hello world!!");
+	   log.debug("debug level!!");
+		return new String("porter!");
+	}
 }
